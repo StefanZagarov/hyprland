b53811388aa1537c2beaaf99f9468f280a4f7eed
StefanZagarov <stefanzagarov@gmail.com>
2025-12-17 11:04:41 +0200

    Treesitter textobject moved to main branch - we not listen to it to get updates

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,107 +1,78 @@
 return {
-	-- Treesitter - Highlight, edit, and navigate code
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	main = "nvim-treesitter.configs", -- Sets main module to use for opts
-	-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-	opts = {
-		ensure_installed = {
-			"bash",
-			"c",
-			"diff",
-			"html",
-			"lua",
-			"luadoc",
-			"markdown",
-			"markdown_inline",
-			"query",
-			"vim",
-			"vimdoc",
-			"javascript",
-			"typescript",
-			"tsx",
-			"json",
-			"css",
-			"regex", -- required by Noice for search highlighting
-			"vue",
-			"scss",
-			"yaml",
-		},
-		-- Autoinstall languages that are not installed
-		auto_install = true,
-		highlight = {
-			enable = true,
-			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-			--  If you are experiencing weird indenting issues, add the language to
-			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { "ruby" },
-		},
-		indent = { enable = true, disable = { "ruby" } },
-	},
-	-- There are additional nvim-treesitter modules that you can use to interact
-	-- with nvim-treesitter. You should go explore a few and see what interests you:
-	--
-	--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-	--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-
-	-- Disabled as I am using nvim-navic
-	{ -- Show current function/class context above cursor (like VS Code breadcrumbs)
-		"nvim-treesitter/nvim-treesitter-context",
-		event = "VeryLazy",
-		opts = {
-			max_lines = 1,
-			multiline_threshold = 20,
-			separator = "", -- or "─"
-			zindex = 20,
-		},
-	},
-
-	--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-	{ -- Syntax-aware text objects (e.g. `af` = a function, `if` = inner function)
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		event = "VeryLazy",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true, -- jump to textobj like targets.vim
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-							["aa"] = "@parameter.outer",
-							["ia"] = "@parameter.inner",
-						},
-						selection_modes = {
-							["@function.outer"] = "V", -- linewise
-							["@class.outer"] = "V",
-						},
-						include_surrounding_whitespace = true,
-					},
-					move = {
-						enable = true,
-						set_jumps = true,
-						goto_next_start = {
-							["]m"] = "@class.outer",
-							["]}"] = "@function.inner",
-							["]]"] = "@function.outer",
-						},
-						goto_previous_start = {
-							["[m"] = "@class.outer",
-							["[{"] = "@function.inner",
-							["[["] = "@function.outer",
-						},
-					},
-					swap = {
-						enable = true,
-						swap_next = { ["<leader>a"] = "@parameter.inner" },
-						swap_previous = { ["<leader>A"] = "@parameter.inner" },
-					},
-				},
-			})
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        opts = {
+            ensure_installed = {
+                "bash", "c", "diff", "html", "lua", "luadoc",
+                "markdown", "markdown_inline", "query", "vim",
+                "vimdoc", "javascript", "typescript", "tsx",
+                "json", "css", "regex", "vue", "scss", "yaml",
+            },
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = { "ruby" },
+            },
+            indent = { enable = true, disable = { "ruby" } },
+            
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["aa"] = "@parameter.outer",
+                        ["ia"] = "@parameter.inner",
+                    },
+                    selection_modes = {
+                        ["@function.outer"] = "V",
+                        ["@class.outer"] = "V",
+                    },
+                    include_surrounding_whitespace = true,
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        ["]m"] = "@class.outer",
+                        ["]}"] = "@function.inner",
+                        ["]]"] = "@function.outer",
+                    },
+                    goto_previous_start = {
+                        ["[m"] = "@class.outer",
+                        ["[{"] = "@function.inner",
+                        ["[["] = "@function.outer",
+                    },
+                },
+                swap = {
+                    enable = true,
+                    swap_next = { ["<leader>a"] = "@parameter.inner" },
+                    swap_previous = { ["<leader>A"] = "@parameter.inner" },
+                },
+            },
+        },
+    },
+    
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        event = "VeryLazy",
+        opts = {
+            max_lines = 1,
+            multiline_threshold = 20,
+            separator = "",
+            zindex = 20,
+        },
+    },
+    
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",  -- ✅ THIS IS THE FIX
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        event = "VeryLazy",
+    },
 }
